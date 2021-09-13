cbe22f4cc4a38493c6db4287e0d948a892656158
iamrivera <a3m3rivera@gmail.com>
2021-09-13 12:27:03 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,50 @@
 // create your App component here
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    state = {
+        peopleInSpace: [],
+    }
+
+    render() { 
+        return <div>{this.state.peopleInSpace.map((person) => person.name)}</div>;
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+            .then((resp) => resp.json())
+            .then((data) => {
+                this.setState({
+                    peopleInSpace: data.people,
+                })
+            })
+    }
+
+    // SAMPLE CODE FROM PREVIOUS LESSON 
+
+    // class App extends Component {
+    //     state = {
+    //       peopleInSpace: [],
+    //     };
+      
+    //     render() {
+    //       return <div>{this.state.peopleInSpace.map((person) => person.name)}</div>;
+    //     }
+      
+    //     componentDidMount() {
+    //       fetch("http://api.open-notify.org/astros.json")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //           this.setState({
+    //             peopleInSpace: data.people,
+    //           });
+    //         });
+    //     }
+    //   }
+
+
+
+
+
+}
